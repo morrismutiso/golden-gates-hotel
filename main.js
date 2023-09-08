a2789c788891579c07b3412a2c37b9778f1b2a7b
Morris <morrismutiso01@gmail.com>
2023-09-08 22:59:06 +0300

    added some files and made some necessary changes

---
+++ main.js
@@ -0,0 +1,43 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+var swiper = new Swiper(".home-slider", {
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+let imgBx = document.querySelectorAll(".imgBx");
+let contentBx = document.querySelectorAll(".contentBx");
+
+for (let i = 0; i < imgBx.length; i++) {
+  imgBx[i].addEventListener("mouseover", function () {
+    for (let i = 0; i < contentBx.length; i++) {
+      contentBx[i].className = "contentBx";
+    }
+    document.getElementById(this.dataset.id).className = "contentBx active";
+
+    for (let i = 0; i < imgBx.length; i++) {
+      imgBx[i].className = "imgBx";
+    }
+    this.className = "imgBx active";
+  });
+}
